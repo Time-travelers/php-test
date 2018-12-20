80d0b71d87533c30128ef4dfa6dd7ba6e65e596c
xu.weishuai <xu.weishuai@xcar.com.cn>
2018-12-20 14:11:56 +0800

    PHP 基础知识 model

---
+++ model/easy-tips-master/easy-tips-master/patterns/bridge/PersonMale.php
@@ -0,0 +1,20 @@
+<?php
+namespace bridge;
+
+/**
+ * 男人实类
+ */
+class PersonMale extends PersonAbstract
+{
+  /**
+   * 吃的具体方式
+   *
+   * @param  string $food 食物
+   * @return string
+   */
+  public function eat($food='')
+  {
+      echo $this->_gender;
+     $this->_tool->eat($food);
+  }
+}

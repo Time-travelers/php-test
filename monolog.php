1044d138a28f80a81d1cf8fac9140e23e3d2fde1
xu.weishuai <xu.weishuai@xcar.com.cn>
2018-12-24 14:10:08 +0800

    monolog 代码测试

--- monolog.php
+++ monolog.php
@@ -2,10 +2,39 @@
     include './vendor/autoload.php';
 	use Monolog\Logger;
 	use Monolog\Handler\StreamHandler;
+	use Monolog\Handler\FirePHPHandler;
 	// 创建日志频道
-	$log = new Logger('name');
-	$log->pushHandler(new StreamHandler('D:\WWW\time-travelers\your.log', Logger::WARNING));
+	$log = new Logger('log');
+	$log_name = new Logger('log_name');
+
+    $json= new \Monolog\Formatter\JsonFormatter();
+//    $es= new \Monolog\Formatter\ElasticaFormatter();
+//    $logStash= new \Monolog\Formatter\LogstashFormatter(); //把日志记录格式化成logstash的事件JSON格式。
+
+
+
+	$stream=new StreamHandler('D:\WWW\time-travelers\your.log', Logger::INFO);
+	$stream_name=new StreamHandler('D:\WWW\time-travelers\your.log', Logger::INFO);
+
+    $stream->setFormatter($json);
+
+    $firePhp=new FirePHPHandler();
+//    $socket=new \Monolog\Handler\SocketHandler();  //通过socket写日志。
+
+	$log->pushHandler($stream);
+    $log->pushHandler($firePhp);
+    $log_name->pushHandler($stream_name);
+
+//Processor可以为日志记录添加额外的信息
+    $log->pushProcessor(function ($record) {
+        $record['extra']['dummy'] = 'Hello world!';
+        return $record;
+    });
 	// 添加日志记录
-	$log->addWarning('Foo');
-	$log->addError('Bar');
-//    $log->addInfo('My logger is now ready');
+	$log->addWarning('Foo',['name'=>'xwsh']);
+	$log->addError('Bar',['name'=>'xwsh']);
+    $log->addInfo('My logger is now ready',['name'=>'xwsh']);
+    $log_name->addInfo('My logger is now ready',['name'=>'xwsh']);
+
+//    $log->addRecord();
+

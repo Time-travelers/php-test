d6ebda08d1d243c5f686c956e7e7f43f1a17f525
xu.weishuai <xu.weishuai@xcar.com.cn>
2019-01-02 16:21:53 +0800

    测试PHP数组函数

--- test.php
+++ test.php
@@ -5,3 +5,22 @@
  * Date: 2019/1/2
  * Time: 15:08
  */
+
+// 填充一個數組
+$temp=array_pad([1,2,3],20,20);
+
+//var_dump($temp);
+
+function add($a,$b){
+    return $a+$b;
+
+}
+$arr=[1,2,3];
+//$arr=[];
+$tt=array_reduce($arr,'add','is null');
+var_dump($tt);
+
+$arr=array_reverse($arr);
+$arr_flip=array_flip($arr);
+var_dump($arr);
+var_dump($arr_flip);

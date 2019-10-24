f7a6f07ce19fac6eb00815a3337dd503b7b1093b
xu.weishuai <xu.weishuai@xcar.com.cn>
2019-10-24 15:14:41 +0800

    剔除python部分

--- phpunit/StackTest.php
+++ phpunit/StackTest.php
@@ -6,11 +6,11 @@
  * Time: 9:40
  */
 
+//当你想把一些东西写到 print 语句或者调试表达式中时，别这么做，将其写成一个测试来代替。
 use PHPUnit\Framework\TestCase;
 
 class StackTest extends TestCase
 {
-
     public function testPushAndPop()
     {
         $stack = [];
@@ -23,4 +23,17 @@ class StackTest extends TestCase
         $this->assertEquals('foo', array_pop($stack));
         $this->assertEquals(0, count($stack));
     }
+    public function testPushAndPop2()
+    {
+        $stack = [];
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(2, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
 }
+?>
